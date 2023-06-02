deeee561a85523fdb4d36bd3ec328a7b1d7ab450
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-06-02 22:30:53 +0900

    socket通信実装

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,23 +1,51 @@
 "use client";
 
-import { useCallback, useEffect } from "react";
+import { useCallback, useState } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("ws://localhost:8080/", {});
+
+socket.on("connect", () => {
+  console.log(`connect ${socket.id}`);
+});
+
+socket.on("disconnect", () => {
+  console.log(`disconnect`);
+});
 
 export default function Home() {
-  const fetchHello = useCallback(async () => {
-    const res = await fetch("/api/hello");
-    console.log(await res.json());
+  const [value, setValue] = useState("");
+  const [list, setList] = useState<string[]>([]);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
   }, []);
 
-  useEffect(() => {
-    fetchHello();
-  }, [fetchHello]);
+  const handleSubmit = useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
+      socket.emit("send_message", value);
+    },
+    [value]
+  );
+
+  socket.on("received_message", (data) => {
+    console.log(data);
+    setList([...list, data]);
+  });
 
   return (
     <main>
-      <div>
-        <p>Get started by editing&nbsp;</p>
-        <div></div>
-      </div>
+      <p>Get started by editing&nbsp;</p>
+      <form onSubmit={handleSubmit}>
+        <input value={value} onChange={handleChange} />
+        <button type="submit">送信</button>
+      </form>
+      <ul>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
     </main>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
 const PORT = 8080;
 const app = express();
 const router = express.Router();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: ["http://localhost:3000"] },
+});
 
 // Middleware
 app.use(express.json());
@@ -15,7 +22,20 @@ router.get(
 );
 app.use("/", router);
 
-app.listen(PORT, () =>
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("send_message", (data) => {
+    console.log(data);
+
+    io.emit("received_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(PORT, () =>
   console.log(`listening on port http://localhost:${PORT}`)
 );
 
