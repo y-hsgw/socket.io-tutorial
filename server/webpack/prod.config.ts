720262b067bedf16142feacd2eea7666a85bda25
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-06-05 20:22:36 +0900

    prod環境の環境変数設定

--- server/webpack/prod.config.ts
+++ server/webpack/prod.config.ts
@@ -1,8 +1,10 @@
+import Dotenv from "dotenv-webpack";
 import baseConfig from "./base.config";
 import merge from "webpack-merge";
 
 const config = merge(baseConfig, {
   mode: "production",
+  plugins: [new Dotenv()],
 });
 
 export default config;
